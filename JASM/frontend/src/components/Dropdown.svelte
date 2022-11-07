58804ef8ddc8bf1097cf86c076b58eb0370a8387
Mangiang <mangiang@orange.fr>
2022-11-06 20:40:01 -0500

    chore: cleanup header css classes

---
+++ JASM/frontend/src/components/Dropdown.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let options;
+  export let value;
+  export let label;
+</script>
+
+<ul class="menu menu-horizontal p-0">
+  <li>
+    <div class="form-control">
+      <select class="select select-bordered w-full max-w-xs" bind:value={$value}>
+        <option disabled>{label}</option>
+        {#each options as option}
+          <option value={option.value}>{option.key}</option>
+        {/each}
+      </select>
+    </div>
+  </li>
+</ul>
--- JASM/frontend/src/components/Header.svelte
+++ JASM/frontend/src/components/Header.svelte
@@ -1,33 +1,27 @@
 <script lang="ts">
   import { refreshRate } from "../stores/generalStore";
   import { RefreshRate } from "../data/refreshRate";
+  import Dropdown from "./Dropdown.svelte";
   const refreshRecord = Object.keys(RefreshRate)
     .map((key) => ({ key, value: RefreshRate[key] }))
     .filter((elt) => !isNaN(elt.value));
 </script>
 
-<div class="navbar bg-base-100">
+<div class="navbar ">
   <div class="flex-1">
-    <a class="btn btn-ghost normal-case text-xl">JASM</a>
+    <a id="brand">JASM</a>
   </div>
   <div class="flex-none">
-    <ul class="menu menu-horizontal p-0">
-      <li>
-        <div class="form-control">
-          <select class="select select-bordered w-full max-w-xs" bind:value={$refreshRate}>
-            <option disabled>Refresh rate</option>
-            {#each refreshRecord as rate}
-              <option value={rate.value}>{rate.key}</option>
-            {/each}
-          </select>
-        </div>
-      </li>
-    </ul>
+    <Dropdown label={"Refresh rate"} options={refreshRecord} value={refreshRate} />
   </div>
 </div>
 
 <style>
   .navbar {
-    @apply shadow-lg;
+    @apply shadow-lg bg-base-100;
+  }
+
+  #brand {
+    @apply btn btn-ghost normal-case text-xl;
   }
 </style>
