3742cacdfb47029e00afa86b6cd2bd2bb0ce9fa4
waffle.lord <waffle.lord@hotmail.com>
2022-11-07 08:19:02 -0500

    remove backend timer, frontend is handling refresh rate

--- JASM/app.go
+++ JASM/app.go
@@ -6,14 +6,11 @@ import (
 	"time"
 
 	"github.com/shirou/gopsutil/v3/cpu"
-	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 // App struct
 type App struct {
-	ctx         context.Context
-	ticker      time.Ticker
-	tickChannel chan bool
+	ctx context.Context
 }
 
 type CpuInfo struct {
@@ -28,43 +25,14 @@ func NewApp() *App {
 	return &App{}
 }
 
-// startup is called when the app starts. The context is saved
+// SetContext is called when the app starts. The context is saved
 // so we can call the runtime methods
 func (a *App) SetContext(ctx context.Context) {
 	a.ctx = ctx
 }
 
-func (a *App) refreshData() {
-	for {
-		select {
-		case tick := <-a.ticker.C:
-			// update frontend data
-			fmt.Printf("Tick  :: %s\n", tick)
-			fmt.Println("Event :: dataRefresh")
-			runtime.EventsEmit(a.ctx, "dataRefresh")
-
-		case <-a.tickChannel:
-			fmt.Println("tickChannel closing")
-			close(a.tickChannel)
-		}
-	}
-}
-
-func (a *App) StartTimer() {
-	a.tickChannel = make(chan bool)
-	a.ticker = *time.NewTicker(time.Second * 1)
-	go a.refreshData()
-	fmt.Println("Refresh Timer Started")
-}
-
-func (a *App) StopTimer() {
-	a.ticker.Stop()
-	a.tickChannel <- true
-	fmt.Println("Refresh Timer Stopped")
-}
-
 func (a *App) GetCpuData() *CpuInfo {
-	fmt.Println("Getting CPU data")
+	fmt.Printf("%v :: Getting CPU data\n", time.Now().Unix())
 
 	info, _ := cpu.Info()
 
